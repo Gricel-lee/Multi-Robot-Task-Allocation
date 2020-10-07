b315bc19a93fa2e89feba6c36cfcb095d64dec66
Gricel Vazquez <63869574+Gricel-lee@users.noreply.github.com>
2020-10-07 17:35:36 +0100

    Add files via upload

---
+++ system_0/config/settings.py
@@ -0,0 +1,17 @@
+
+
+DSL_SYSTEM_DECL = 'C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\dsl_system_declaration\\DSL_System_Declaration.txt'
+
+HDDL_DOMAIN = 'C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\hddl_system_declaration\\domain\\VisitRoom-problem2.hddl'
+HDDL_PROBLEM ='C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\hddl_system_declaration\\problem\\Hospital.hddl'
+
+
+ROBOTS_DB = 'C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\db\\Robots_db.xml'
+TASK_STATUS=['pending', 'scheduled', 'executing', 'finished']
+
+
+SIMULATION = True
+
+ALLOY_OUTPUT = 'C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\models\\alloy_example_output.xml'
+
+ROBOTS_AUGMENTED_DB = 'C:\\Users\\grist\\eclipse-workspace\\SampleV2-MultiRobot-Task-Allocation\\system_0\\db\\dbgenerated\\Robots_db.xml'
